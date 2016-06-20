8eb0d517bd4ca615d5606e89b5217276a36f91be
Alex Trotsenko <olexii.trotsenko@gmail.com>
2016-06-20 16:10:47 +0200

    1. RxJava was added for getting text input at search and getting books info from rest API for this text input. 2. Retrolambda was added in order to reduce amount of boilerplate code

--- app/src/main/java/com/alextrotsenko/booksearch/BookSearchActivityFragment.java
+++ app/src/main/java/com/alextrotsenko/booksearch/BookSearchActivityFragment.java
@@ -4,28 +4,35 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.alextrotsenko.booksearch.rest.MetaData;
 import com.alextrotsenko.booksearch.rest.dto.BooksInfo;
 import com.alextrotsenko.booksearch.rest.services.GoogleBookService;
+import com.jakewharton.rxbinding.widget.RxTextView;
+
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnTextChanged;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  *  1st screen with the list of google book, matching search request.
  */
 public class BookSearchActivityFragment extends Fragment {
+
+    @BindView(R.id.book_search_input)
+    EditText bookSearchInput;
 
     @BindView(R.id.books)
     RecyclerView books;
@@ -42,6 +49,7 @@ public class BookSearchActivityFragment extends Fragment {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(MetaData.GOOGLE_BOOKS_BASE_URL)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
@@ -54,22 +62,20 @@ public class BookSearchActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_book_search, container, false);
         ButterKnife.bind(this, view);
 
+        RxTextView.textChanges(bookSearchInput)
+                .debounce(1, TimeUnit.SECONDS)
+                .filter(inputText -> !TextUtils.isEmpty(inputText))
+                .flatMap(inputText -> booksService.getBooks(inputText).subscribeOn(Schedulers.io()))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        onNext -> displayNewBooks(onNext),
+                        error -> Log.e("AlexT", "error at request to rest api:", error)
+                );
+
         return view;
     }
 
-    @OnTextChanged(R.id.book_search_input)
-    public void onSearchTextChanged(CharSequence lookupText) {
-        final Call<BooksInfo> booksCall = booksService.getBooks(lookupText);
-        booksCall.enqueue(new Callback<BooksInfo>() {
-            @Override
-            public void onResponse(Call<BooksInfo> call, Response<BooksInfo> response) {
-                Log.e("Alex", "result: " + response.body());
-            }
-
-            @Override
-            public void onFailure(Call<BooksInfo> call, Throwable t) {
-                Log.e("Alex", "error: " + t);
-            }
-        });
+    private int displayNewBooks(BooksInfo onNext) {
+        return Log.e("AlexTr", "rest request result: " + onNext);
     }
 }
--- app/src/main/java/com/alextrotsenko/booksearch/rest/services/GoogleBookService.java
+++ app/src/main/java/com/alextrotsenko/booksearch/rest/services/GoogleBookService.java
@@ -3,14 +3,14 @@ package com.alextrotsenko.booksearch.rest.services;
 import com.alextrotsenko.booksearch.rest.MetaData;
 import com.alextrotsenko.booksearch.rest.dto.BooksInfo;
 
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Server side rest API for getting books data.
  */
 public interface GoogleBookService {
     @GET(MetaData.VolumesList.PATH)
-    Call<BooksInfo> getBooks(@Query(MetaData.VolumesList.SEARCH_QUERY_PARAM) CharSequence bookName);
+    Observable<BooksInfo> getBooks(@Query(MetaData.VolumesList.SEARCH_QUERY_PARAM) CharSequence bookName);
 }
